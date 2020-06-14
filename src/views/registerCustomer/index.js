820868a12cba57884d0be7b638357e6812d7c85a
Alik Rodrigues <alik.rodrigues@pluks.com.br>
2020-06-14 13:35:31 -0300

    implementado lottie.

--- src/views/registerCustomer/index.js
+++ src/views/registerCustomer/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Template from '../../components/template';
 import {View} from 'react-native';
-import {Title, TextInput, Button} from 'react-native-paper';
+import {Title, TextInput} from 'react-native-paper';
 import CustomButton from '../../components/customButton';
+import {Actions} from 'react-native-router-flux';
+import LottieView from 'lottie-react-native';
 
 const RegisterCustomer = () => {
+  const [showSuccess, setShowSuccess] = useState(false);
+
+  if (showSuccess) {
+    setTimeout(() => {
+      Actions.mainPage();
+    }, 5000);
+
+    return (
+      <LottieView
+        source={require('../../assets/animations/sucess.json')}
+        autoPlay
+        loop
+      />
+    );
+  }
+
   return (
     <Template title={'Cadastro'}>
       <Title style={{textAlign: 'center', marginVertical: 10, color: '#fff'}}>
@@ -22,7 +40,12 @@ const RegisterCustomer = () => {
           onChangeText={text => {}}
         />
       </View>
-      <CustomButton title={'Agendar'} onPress={() => {}} />
+      <CustomButton
+        title={'Agendar'}
+        onPress={() => {
+          setShowSuccess(true);
+        }}
+      />
     </Template>
   );
 };
